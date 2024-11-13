5b30726c031d7064cd262856914d720c5ec46405
wehamin62jau <hashamd25@gmail.com>
2024-11-13 20:11:10 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,6 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-MONGODB: process.env.MONGODB || "",
+MONGODB: process.env.MONGODB || "mongodb://mongo:ULmtVjjJNoyPgHVTQCkfeqvRnCyYqptG@junction.proxy.rlwy.net:35481",
 
 };
